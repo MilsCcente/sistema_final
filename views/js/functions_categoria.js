5793e5b07c89b3660d9758882f41b4529c8c9092
MilsCcente <ccenterimachimiluska@gmail.com>
2024-12-12 12:19:39 -0500

    Update functions_categoria.js

--- views/js/functions_categoria.js
+++ views/js/functions_categoria.js
@@ -221,7 +221,7 @@ async function fnt_eliminar(id) {
 
         if (json.status) {
             swal("Eliminar", json.message || "Eliminado correctamente", "success");
-            document.querySelector('#fila' + id).remove();
+            document.querySelector('#fila_' + id).remove();
         } else {
             swal("Eliminar", json.message, "warning");
         }
